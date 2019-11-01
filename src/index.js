57548d5684d11b6df0de9ecc7c6467ef3fb7967b
siedgustavo@gmail.com <siedgustavo@gmail.com>
2019-11-01 20:13:29 -0300

    done server

---
+++ src/index.js
@@ -0,0 +1,52 @@
+const express =require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const myUser = {
+    email: "siedgustavo@gmail.com",
+    password: "123456"
+}
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.status(200).send("server working")
+})
+
+app.post("/login", (req, res) => {
+
+    console.log(req.body)
+
+    if(!req.body.email){
+        return res.status(400).send({
+            success: false,
+            message: "Email is required"
+        })
+    }
+
+    if(!req.body.password){
+        return res.status(400).send({
+            success: false,
+            message: "Password is required"
+        })
+    }
+
+    if(req.body.email !== myUser.email || req.body.password !== myUser.password){
+
+        return res.status(400).send({
+            success: false,
+            message: "User not exist"
+        })
+
+    }
+
+    res.status(200).send("server working")
+  
+})
+
+app.listen(4000, () => {
+    console.log("Server Working")
+})
+
